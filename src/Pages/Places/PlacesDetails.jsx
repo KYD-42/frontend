850d76c2292ff4a7cd6d6be740f37ebfeff5f309
“Filipe.Santiago” <thekyd42@gmail.com>
2024-03-14 16:54:22 +0000

    16h55

--- src/Pages/Places/PlacesDetails.jsx
+++ src/Pages/Places/PlacesDetails.jsx
@@ -1,4 +1,4 @@
-/* import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
 import axios from "axios";
 
@@ -147,7 +147,9 @@ function PlaceDetails() {
 }
 
 export default PlaceDetails;
- */
+
+
+/*
 
 import React, { useState, useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
@@ -293,3 +295,5 @@ function PlaceDetails() {
 }
 
 export default PlaceDetails;
+
+ */
